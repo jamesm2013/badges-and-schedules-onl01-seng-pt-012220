27e03a93d27572bdd81439a717642b2c2feabac1
james Johnson <2013matthew2013@gmail.com>
2020-01-29 02:50:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,4 +8,4 @@ end
  def assign_rooms (speakers)
    new_arr = []
    speakers.each_with_index do | name , i|
-     new_arr<< "Hello, #{name}! You'll be assign_rooms
+     new_arr<< "Hello, #{name}! You'll be assign to room 
